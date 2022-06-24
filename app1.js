de70fe4a7ed8318d5959eb2d9eefb53a5b830439
ParagRawal46179091 <61102264+rawalparag6@users.noreply.github.com>
2022-06-24 15:28:35 +0530

    Delete app1.js

--- app1.js
+++
@@ -1,5 +0,0 @@
-var counter = function(arr){
-    return 'There are' + ' ' + arr.length + ' ' +'elements in this array';
-    };
-    
-    module.exports = counter; //export counter so we can use outside scopr of particular class
