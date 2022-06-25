b4c78d3bd639a7d527fdb8faf6893578bb34ed91
ParagRawal46179091 <61102264+rawalparag6@users.noreply.github.com>
2022-06-25 13:49:30 +0530

    Reading & Writing Files (FS)

---
+++ app.js
@@ -0,0 +1,13 @@
+// var fs = require('fs');
+// var readMe= fs.readFileSync('readMe.txt','utf8');
+// console.log(readMe);
+
+// var fs = require('fs');
+// var readMe= fs.readFileSync('readMe.txt','utf8');
+// console.log(readMe);
+// fs.writeFileSync('writeMe.txt',readMe);
+// var fs = require('fs');
+
+// fs.readFile('readMe.txt', 'utf8', function(err, data){
+//     fs.writeFile('writeMe.txt',data);
+// });
